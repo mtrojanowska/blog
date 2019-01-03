3f6e6948de4474b03ea9a183ce79ecaf4b0bb30c
Marta Trojanowska <m.troyanovska@gmail.com>
2019-01-03 19:53:52 +0100

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181229170907) do
+ActiveRecord::Schema.define(version: 20190103180117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20181229170907) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
