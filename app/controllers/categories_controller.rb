997b7be3a1c6857da884dee0a732837977623f7c
Marta Trojanowska <m.troyanovska@gmail.com>
2019-03-17 08:43:57 +0100

    faulty restrictions fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :require_admin, only: [:index, :show]
+  before_action :require_admin, except: [:index, :show]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-
   root 'pages#home'
 
   get 'pages/about', to: 'pages#about'
@@ -16,10 +14,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-
-resources :categories, except: [:destroy]
-
-
-
+  resources :categories, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
